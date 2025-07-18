eec282cc025184f8a05ec27ee4e6172305cb2c06
Jan Pabisiak <contact@janpabisiak.com>
2025-07-19 01:24:36 +0200

    add error-page with wildcart route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from "@angular/router";
 import { HomePageComponent } from "./components/home-page/home-page.component";
 import { QuizPageComponent } from "./components/quiz-page/quiz-page.component";
+import { ErrorPageComponent } from "./components/error-page/error-page.component";
 
 export const routes: Routes = [
 	{
@@ -10,5 +11,9 @@ export const routes: Routes = [
 	{
 		path: "quiz",
 		component: QuizPageComponent,
+	},
+	{
+		path: "**",
+		component: ErrorPageComponent,
 	},
 ];
