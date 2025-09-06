3358cc08eac0ad9bc591ed5f1cb62291d422ec27
Jan Pabisiak <contact@janpabisiak.com>
2025-09-07 01:31:40 +0200

    allow sorting by isLearning state

--- src/app/components/home-page/word-list/word-list-options/word-list-sort/word-list-sort.component.ts
+++ src/app/components/home-page/word-list/word-list-options/word-list-sort/word-list-sort.component.ts
@@ -1,16 +1,31 @@
-import { Component, HostListener } from "@angular/core";
+import { Component, HostListener, OnDestroy, OnInit } from "@angular/core";
 import { ESortTypes, WordsService } from "@services/words.service";
+import { Subject, takeUntil } from "rxjs";
 
 @Component({
 	selector: "app-word-list-sort",
 	imports: [],
 	templateUrl: "./word-list-sort.component.html",
 })
-export class WordListSortComponent {
+export class WordListSortComponent implements OnInit, OnDestroy {
+	private destroy$ = new Subject<void>();
+	hasNotLearningWords = false;
+
 	@HostListener("change", ["$event.target"])
 	sortWordList(hostEl: HTMLSelectElement) {
 		this.wordsService.sortWordList(hostEl.value as ESortTypes);
 	}
 
 	constructor(private wordsService: WordsService) {}
+
+	ngOnInit() {
+		this.wordsService.filteredWordList$.pipe(takeUntil(this.destroy$)).subscribe((wordList) => {
+			this.hasNotLearningWords = wordList.some((w) => !w.isLearning);
+		});
+	}
+
+	ngOnDestroy() {
+		this.destroy$.next();
+		this.destroy$.complete();
+	}
 }
