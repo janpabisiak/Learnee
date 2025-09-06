7d8fa1d05da50a0251f7cd2de771241e32670e7c
Jan Pabisiak <contact@janpabisiak.com>
2025-09-07 01:29:18 +0200

    make progress bar common for all game types

--- src/app/components/study-page/study-page.component.ts
+++ src/app/components/study-page/study-page.component.ts
@@ -1,10 +1,41 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { EAvailableGames, GameService, IStage } from "@services/game.service";
+import { combineLatest, Subject, takeUntil } from "rxjs";
 import { MatchingGameComponent } from "./matching-game/matching-game.component";
 import { QuizGameComponent } from "./quiz-game/quiz-game.component";
+import { StudyProgressBarComponent } from "./study-progress-bar/study-progress-bar.component";
 
 @Component({
 	selector: "app-study-page",
-	imports: [MatchingGameComponent, QuizGameComponent],
+	imports: [QuizGameComponent, MatchingGameComponent, StudyProgressBarComponent],
 	templateUrl: "./study-page.component.html",
+	providers: [GameService],
 })
-export class StudyPageComponent {}
+export class StudyPageComponent implements OnInit, OnDestroy {
+	private destroy$ = new Subject<void>();
+	stages: IStage[] = [];
+	currentStageId: number = 0;
+	EAvailableGames = EAvailableGames;
+
+	constructor(private gameService: GameService) {}
+
+	ngOnInit() {
+		this.gameService.generateStages();
+
+		combineLatest([this.gameService.stages$, this.gameService.currentStageId$])
+			.pipe(takeUntil(this.destroy$))
+			.subscribe(([stages, currentId]) => {
+				this.stages = stages;
+				this.currentStageId = currentId;
+			});
+	}
+
+	getCurrentStage() {
+		return this.stages[this.currentStageId];
+	}
+
+	ngOnDestroy() {
+		this.destroy$.next();
+		this.destroy$.complete();
+	}
+}
