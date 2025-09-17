1df1146e81c24a83087164a4f64c89930a3d607f
Jan Pabisiak <contact@janpabisiak.com>
2025-09-17 13:05:13 +0200

    create level progress component

---
+++ src/app/components/home-page/user-statistics/heat-map/heat-map.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { IStatistics, LevelService } from "@services/level.service";
+import { Subject, takeUntil } from "rxjs";
+import { HeatMapItemComponent } from "./heat-map-item/heat-map-item.component";
+
+@Component({
+	selector: "app-heat-map",
+	imports: [HeatMapItemComponent],
+	templateUrl: "./heat-map.component.html",
+})
+export class HeatMapComponent implements OnInit, OnDestroy {
+	statistics: IStatistics[] = [];
+	maxPlays = 0;
+	private destroy$ = new Subject<void>();
+
+	constructor(private levelService: LevelService) {}
+
+	ngOnInit() {
+		this.levelService.statistics$.pipe(takeUntil(this.destroy$)).subscribe((statistics) => {
+			this.statistics = Array.from(statistics).map(([date, numberOfPlays]) => ({
+				date,
+				numberOfPlays,
+			}));
+
+			this.maxPlays = Math.max(...this.statistics.map((day) => day.numberOfPlays));
+		});
+	}
+
+	ngOnDestroy() {
+		this.destroy$.next();
+		this.destroy$.complete();
+	}
+}
