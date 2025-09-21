0b568a54fd7ccdadeb3d90fe3541b11e8edcbcbf
Jan Pabisiak <contact@janpabisiak.com>
2025-09-21 23:56:17 +0200

    add progress bar to results component

--- src/app/components/study-page/game-results/game-results.component.ts
+++ src/app/components/study-page/game-results/game-results.component.ts
@@ -3,14 +3,17 @@ import { SectionTitleComponent } from "@components/utils/section-title/section-t
 import { EAvailableGames, GameService, IStage } from "@services/game.service";
 import { Subject, takeUntil } from "rxjs";
 import { GameContainerComponent } from "../game-section/game-container/game-container.component";
+import { ProgressBarComponent } from "@components/utils/progress-bar/progress-bar.component";
 
 @Component({
 	selector: "app-game-results",
-	imports: [SectionTitleComponent, GameContainerComponent],
+	imports: [SectionTitleComponent, GameContainerComponent, ProgressBarComponent],
 	templateUrl: "./game-results.component.html",
 })
 export class GameResultsComponent implements OnInit, OnDestroy {
 	stages: IStage[] = [];
+	correctAnswerAmount = 0;
+	correctAnswerPercentage = 0;
 	EAvailableGames = EAvailableGames;
 	private destroy$ = new Subject<void>();
 
@@ -19,6 +22,12 @@ export class GameResultsComponent implements OnInit, OnDestroy {
 	ngOnInit() {
 		this.gameService.stages$.pipe(takeUntil(this.destroy$)).subscribe((stages) => {
 			this.stages = stages;
+			this.correctAnswerAmount = stages.reduce(
+				(acc, stage) => acc + +(stage.answeredCorrect === true),
+				0
+			);
+			this.correctAnswerPercentage =
+				Math.round(this.correctAnswerAmount / stages.length) * 100;
 		});
 	}
 
--- src/app/components/utils/progress-bar/progress-bar.component.ts
+++ src/app/components/utils/progress-bar/progress-bar.component.ts
@@ -1,11 +1,19 @@
+import { NgClass } from "@angular/common";
 import { Component, Input } from "@angular/core";
 
 @Component({
 	selector: "app-progress-bar",
-	imports: [],
+	imports: [NgClass],
 	templateUrl: "./progress-bar.component.html",
 })
 export class ProgressBarComponent {
 	@Input({ required: true }) progress!: number;
+	@Input() type: string = EProgressBarTypes.Default;
 	@Input() tooltipContent!: string;
+	EProgressBarTypes = EProgressBarTypes;
+}
+
+export enum EProgressBarTypes {
+	Default = "default",
+	GameResults = "gameResults",
 }
