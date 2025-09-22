f2917c1549da8db35c178faefbeab3879327e612
Jan Pabisiak <contact@janpabisiak.com>
2025-09-22 23:44:06 +0200

    style active link in different way

--- src/app/components/header/header-navbar/header-navbar.component.ts
+++ src/app/components/header/header-navbar/header-navbar.component.ts
@@ -1,12 +1,14 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { HeaderNavbarItemComponent } from "./header-navbar-item/header-navbar-item.component";
+import { ActivatedRoute, NavigationStart, Route, Router } from "@angular/router";
 
 @Component({
 	selector: "app-header-navbar",
 	imports: [HeaderNavbarItemComponent],
 	templateUrl: "./header-navbar.component.html",
 })
-export class HeaderNavbarComponent {
+export class HeaderNavbarComponent implements OnInit {
+	currentUrl: string = "";
 	navLinks = [
 		{
 			id: 0,
@@ -27,4 +29,14 @@ export class HeaderNavbarComponent {
 			icon: "school-outline",
 		},
 	];
+
+	constructor(private router: Router) {}
+
+	ngOnInit() {
+		this.router.events.subscribe((e) => {
+			if (e instanceof NavigationStart) {
+				this.currentUrl = e.url;
+			}
+		});
+	}
 }
