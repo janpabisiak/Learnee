bb73f99e31232d554d59cf82d509cd48d25310d5
Jan Pabisiak <contact@janpabisiak.com>
2025-09-23 12:25:56 +0200

    create exportData and deleteData methods

--- src/app/services/local-storage.service.ts
+++ src/app/services/local-storage.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from "@angular/core";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable({
 	providedIn: "root",
 })
 export class LocalStorageService {
+	private hasKeys = new BehaviorSubject<boolean>(false);
+
+	hasKeys$ = this.hasKeys.asObservable();
+
 	loadData(key: string) {
 		const value = localStorage.getItem(key);
 
@@ -13,5 +18,37 @@ export class LocalStorageService {
 
 	saveData(key: string, value: {}) {
 		localStorage.setItem(key, JSON.stringify(value));
+		this.hasKeys.next(true);
+	}
+
+	exportData() {
+		const data: Record<string, string> = {};
+
+		for (let i = 0; i < localStorage.length; i++) {
+			const key = localStorage.key(i)!;
+			if (!key && key === "key") continue;
+
+			data[key] = this.loadData(key);
+		}
+
+		const json = JSON.stringify(data, null, 2);
+
+		const blob = new Blob([json], { type: "application/json" });
+		const fileURL = URL.createObjectURL(blob);
+
+		const downloadLink = document.createElement("a");
+		downloadLink.href = fileURL;
+		downloadLink.download = "data.json";
+		downloadLink.classList.add("hidden");
+
+		document.body.appendChild(downloadLink);
+		downloadLink.click();
+
+		URL.revokeObjectURL(fileURL);
+	}
+
+	deleteData() {
+		localStorage.clear();
+		this.hasKeys.next(false);
 	}
 }
