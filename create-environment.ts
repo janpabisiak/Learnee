4f9641fd5ba7dcfdff7cde28fb6c9c74ccdd6993
Jan Pabisiak <contact@janpabisiak.com>
2026-02-09 18:46:15 +0100

    create env setup file

---
+++ create-environment.ts
@@ -0,0 +1,36 @@
+import "dotenv/config";
+import fs from "fs";
+import path from "path";
+
+const ENVIRONMENT_FILE_PATH = "./src/environment/environment.prod.ts";
+
+const directory = path.dirname(ENVIRONMENT_FILE_PATH);
+if (!fs.existsSync(directory)) {
+	fs.mkdirSync(directory, { recursive: true });
+}
+
+interface IEnvironmentVariables {
+	production: boolean;
+	apiHost: string;
+	apiUrl: string;
+	apiKey: string;
+}
+
+const environmentVariables: IEnvironmentVariables = {
+	production: process.env["PRODUCTION"] === "true",
+	apiHost: process.env["API_HOST"] ?? "",
+	apiUrl: process.env["API_URL"] ?? "",
+	apiKey: process.env["API_KEY"] ?? "",
+};
+
+fs.writeFileSync(
+	ENVIRONMENT_FILE_PATH,
+	`
+        export const environment = {
+            production: ${environmentVariables.production},
+            apiHost: "${environmentVariables.apiHost}",
+            apiUrl: "${environmentVariables.apiUrl}",
+            apiKey: "${environmentVariables.apiKey}",
+        };
+    `,
+);
