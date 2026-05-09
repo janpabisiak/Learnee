8481a28f3ce4da781aedd9c2d4e3a3c7c2ff1a37
Jan Pabisiak <contact@janpabisiak.com>
2026-05-09 16:22:37 +0200

    fix(wordlist): fix words counter

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,11 +1,9 @@
-import { NgIf } from "@angular/common";
 import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnDestroy, OnInit } from "@angular/core";
 import { SectionTitleComponent } from "@components/utils/section-title/section-title.component";
-import { TranslateService } from "@ngx-translate/core";
+import { TranslatePipe } from "@ngx-translate/core";
 import { EModalType, ModalService } from "@services/modal/modal.service";
-import { combineLatest, Subscription } from "rxjs";
+import { Subscription } from "rxjs";
 import { WordsService } from "../../services/words/words.service";
-import { IWord } from "../../types/word.interface";
 import { UserStatisticsComponent } from "./user-statistics/user-statistics.component";
 import { WordListComponent } from "./word-list/word-list.component";
 import { WordsOfTheDayComponent } from "./words-of-the-day/words-of-the-day.component";
@@ -19,36 +17,21 @@ import { WordsOfTheDayComponent } from "./words-of-the-day/words-of-the-day.comp
 		WordsOfTheDayComponent,
 		UserStatisticsComponent,
 		SectionTitleComponent,
-		NgIf,
+		TranslatePipe,
 	],
 	schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomePageComponent implements OnInit, OnDestroy {
 	private subscription = new Subscription();
 	modalService = inject(ModalService);
-	wordList: IWord[] = [];
+	numOfWords = 0;
 	translations: Record<string, string> | null = null;
 
-	constructor(
-		private wordsService: WordsService,
-		private translation: TranslateService,
-	) {}
+	constructor(private wordsService: WordsService) {}
 
 	ngOnInit() {
-		this.subscription = combineLatest([
-			this.wordsService.wordList$,
-			this.translation.get(["wordlist.title", "wordlist.count", "wordlist.addNew"]),
-		]).subscribe(([wordList, translations]) => {
-			this.wordList = wordList;
-
-			this.translations = translations;
-
-			if (this.translations && "wordlist.count" in this.translations) {
-				this.translations["wordlist.count"] = this.translations["wordlist.count"].replace(
-					"{{count}}",
-					this.wordList.length.toString(),
-				);
-			}
+		this.subscription = this.wordsService.wordList$.subscribe((wordList) => {
+			this.numOfWords = wordList.length;
 		});
 	}
 
